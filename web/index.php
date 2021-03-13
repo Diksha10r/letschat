26cd92ae02160aaaae0a69c9f6227a657566fc06
Diksha10r <diksha10r@gmail.com>
2021-03-14 00:47:01 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,4 +1,4 @@
-$app->get('/cowsay', function() use($app) {
-  $app['monolog']->addDebug('cowsay');
-  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return str_repeat('Hello', getenv('TIMES'));
 });
